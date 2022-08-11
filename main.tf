06099c8791f927c85d50bf32e34a015f97d69633
BuildingOnCloud <72888433+BuildingOnCloud@users.noreply.github.com>
2022-08-11 19:50:04 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+ backend "remote" {
+   # The name of your Terraform Cloud organization.
+   organization = "BuildingOnCloud"
+
+   # The name of the Terraform Cloud workspace to store Terraform state files in.
+   workspaces {
+     name = "Lattice_Test"
+   }
+ }
+}
+
+# An example resource that does nothing.
+resource "null_resource" "example" {
+ triggers = {
+   value = "A example resource that does nothing!"
+ }
+}
