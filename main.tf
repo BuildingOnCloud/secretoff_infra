977117dec01f14e5bb061ec437ce04b56dbbfb91
BuildingOnCloud <72888433+BuildingOnCloud@users.noreply.github.com>
2022-08-12 20:48:31 -0400

    Add provider setting

--- main.tf
+++ main.tf
@@ -9,8 +9,15 @@ terraform {
     }
   }
 }
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
 
-provider "aws" {}
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
 
 resource "aws_ecr_repository" "foo" {
   name                 = "bar"
