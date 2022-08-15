144fb63883c52c53147f446ec32f830edb5cafa5
BuildingOnCloud <72888433+BuildingOnCloud@users.noreply.github.com>
2022-08-15 16:38:47 -0400

    Add docker registry definition

--- main.tf
+++ main.tf
@@ -20,3 +20,13 @@ resource "null_resource" "example" {
 variable "AWS_SECRET_ACCESS_KEY" {
   type = string
 }
+
+# Docker Registry
+resource "aws_ecr_repository" "secretoff_registry" {
+  name                 = "app_registry1"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
