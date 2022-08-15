711ac88c338f00e6ac44f212cc5c9f187011347d
BuildingOnCloud <72888433+BuildingOnCloud@users.noreply.github.com>
2022-08-15 06:05:38 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,12 @@
 terraform {
-  backend "remote" {
-    # The name of your Terraform Cloud organization.
-    organization = "BuildingOnCloud"
-    required_providers {
-        aws = {
-          source = "hashicorp/aws"
-          version = "3.74.2"
-        }
-      }
-
-    # The name of the Terraform Cloud workspace to store Terraform state files in.
-    workspaces {
-      name = "Lattice_Test"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
 }
+
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
@@ -23,13 +15,4 @@ provider "aws" {
 # Create a VPC
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_ecr_repository" "foo" {
-  name                 = "bar"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
 }
