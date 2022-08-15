d1757277641826b196e62b2299ea0f7c17f0b443
BuildingOnCloud <72888433+BuildingOnCloud@users.noreply.github.com>
2022-08-15 18:21:34 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,16 +13,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
 variable "AWS_SECRET_ACCESS_KEY" {
   type = string
 }
 
 # Docker Registry
 resource "aws_ecr_repository" "secretoff_registry" {
-  name = "app_registry1"
-
+  name                 = "app_registry1"
+  image_tag_mutability = "MUTABLE"
+  
   image_scanning_configuration {
     scan_on_push = true
   }
 }
+
