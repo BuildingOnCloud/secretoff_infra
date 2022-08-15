cf835e84f32b32857e21fddbf8541ff48c496224
BuildingOnCloud <72888433+BuildingOnCloud@users.noreply.github.com>
2022-08-15 16:42:58 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,13 +10,6 @@ terraform {
   }
 }
 
-# An example resource that does nothing.
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
-  }
-}
-
 variable "AWS_SECRET_ACCESS_KEY" {
   type = string
 }
