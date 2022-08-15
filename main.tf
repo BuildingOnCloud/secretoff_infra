5c43dbb098358d33ee19a9542b0c577f04c59bb5
BuildingOnCloud <72888433+BuildingOnCloud@users.noreply.github.com>
2022-08-15 16:21:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,13 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.74.1"
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "BuildingOnCloud"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "Lattice_Test"
     }
   }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
 }
 
 # Create a VPC
