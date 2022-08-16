34cb36d6a5dfb19b7852038d46eff4a471f6c23e
BuildingOnCloud <72888433+BuildingOnCloud@users.noreply.github.com>
2022-08-16 14:49:33 -0400

    Fix Container Name

--- ecs.tf
+++ ecs.tf
@@ -7,7 +7,7 @@ resource "aws_ecs_task_definition" "service_secretoff" {
   execution_role_arn       = "arn:aws:iam::224270744698:role/ecsTaskExecutionRole"
   container_definitions = jsonencode([
     {
-      name      = "app_ecs-service1"
+      name      = "app_container1"
       image     = "224270744698.dkr.ecr.us-east-1.amazonaws.com/app_registry1"
       cpu       = 1
       memory    = 512
