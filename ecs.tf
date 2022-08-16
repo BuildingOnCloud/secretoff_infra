8d096b245f11ae5e5357853f58df1a7609435093
BuildingOnCloud <72888433+BuildingOnCloud@users.noreply.github.com>
2022-08-16 16:13:17 -0400

    Add rollback

--- ecs.tf
+++ ecs.tf
@@ -27,9 +27,6 @@ resource "aws_ecs_service" "secretoff_service" {
   cluster         = aws_ecs_cluster.secretoff_cluster.id
   task_definition = aws_ecs_task_definition.service_secretoff.arn
   desired_count   = 2
-  deployment_circuit_breaker {
-    enable = true
-  }
   load_balancer {
     target_group_arn = aws_lb_target_group.secretoff_target_security_group.arn
     container_name   = "app_container1"
@@ -42,5 +39,6 @@ resource "aws_ecs_service" "secretoff_service" {
   }
   deployment_circuit_breaker {
     enable = true
+    rollback = false
   }
 }
