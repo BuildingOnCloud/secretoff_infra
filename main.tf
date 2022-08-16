d874688e9805ecfa4b3cd8f6ee03ca3641ed05c0
BuildingOnCloud <72888433+BuildingOnCloud@users.noreply.github.com>
2022-08-15 20:21:58 -0400

    Add cluster and cloudwatch config

--- main.tf
+++ main.tf
@@ -25,3 +25,30 @@ resource "aws_ecr_repository" "secretoff_registry" {
     scan_on_push = true
   }
 }
+
+
+resource "aws_kms_key" "secretoff_kms_key" {
+  description             = "KMS for secretoff app cloudwatch config"
+  deletion_window_in_days = 7
+}
+
+# CloudWathc Config
+resource "aws_cloudwatch_log_group" "app_cloudwatch" {
+  name = "secretoff_app_logroup"
+}
+
+resource "aws_ecs_cluster" "secretoff_cluster" {
+  name = "app_cluster1"
+
+  configuration {
+    execute_command_configuration {
+      kms_key_id = aws_kms_key.secretoff_kms_key.arn
+      logging    = "OVERRIDE"
+
+      log_configuration {
+        cloud_watch_encryption_enabled = true
+        cloud_watch_log_group_name     = aws_cloudwatch_log_group.app_cloudwatch.name
+      }
+    }
+  }
+}
