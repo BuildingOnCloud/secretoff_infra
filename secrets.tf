e28ebff73a543c5544602f2931674ffebabe0ddd
BuildingOnCloud <72888433+BuildingOnCloud@users.noreply.github.com>
2022-08-16 16:59:17 -0400

    Update secrets.tf

--- secrets.tf
+++ secrets.tf
@@ -1,19 +1,19 @@
 # Secret 1
 resource "aws_secretsmanager_secret" "secretoff_secret1" {
-   name = "APP_PORT"
+  name = "APP_PORT"
 }
  
 resource "aws_secretsmanager_secret_version" "secretversion1" {
-   secret_id     = aws_secretsmanager_secret.secretoff_secret1.id
-   secret_string = "8080"
+  secret_id     = aws_secretsmanager_secret.secretoff_secret1.id
+  secret_string = "8080"
 }
 
 # Secret 2
 resource "aws_secretsmanager_secret" "secretoff_secret2" {
-   name = "API_KEY"
+  name = "API_KEY"
 }
  
 resource "aws_secretsmanager_secret_version" "secretversion2" {
-   secret_id     = aws_secretsmanager_secret.secretoff_secret2.id
-   secret_string = "Secret Off"
+  secret_id     = aws_secretsmanager_secret.secretoff_secret2.id
+  secret_string = "Secret Off"
 }
