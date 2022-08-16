b9d2bff35ac9631e86577354ca778dcee822fc61
BuildingOnCloud <72888433+BuildingOnCloud@users.noreply.github.com>
2022-08-16 10:45:21 -0400

    Initial commit

---
+++ ecs.tf
@@ -0,0 +1,18 @@
+resource "aws_ecs_task_definition" "service_secretoff" {
+  family                = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "app_ecs-service1"
+      image     = "224270744698.dkr.ecr.us-east-1.amazonaws.com/app_registry1"
+      cpu       = 1
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
