92d208691b1257166660457894294e98eb184ad4
BuildingOnCloud <72888433+BuildingOnCloud@users.noreply.github.com>
2022-08-16 17:12:24 -0400

    Update secrets.tf

--- secrets.tf
+++ secrets.tf
@@ -17,3 +17,11 @@ resource "aws_secretsmanager_secret_version" "secretversion2" {
   secret_id     = aws_secretsmanager_secret.secretoff_secret2.id
   secret_string = "Secret Off"
 }
+
+resource "aws_secretsmanager_secret" "example" {
+  name = "example"
+}
+resource "aws_secretsmanager_secret_version" "example" {
+  secret_id     = aws_secretsmanager_secret.example.id
+  secret_string = "example-string-to-protect"
+}
